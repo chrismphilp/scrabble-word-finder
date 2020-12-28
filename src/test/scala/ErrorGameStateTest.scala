2774bda464ab99ed3ee55c7c70034f2598275654
Christopher Philp <chrismphilp@hotmail.co.uk>
2020-12-28 17:31:13 +0000

    [main] | Still need to refactor algorithm, added additional test case

--- src/test/scala/ErrorGameStateTest.scala
+++ src/test/scala/ErrorGameStateTest.scala
@@ -54,4 +54,39 @@ class ErrorGameStateTest extends AnyFunSuite {
     assert(board.boardTiles(10)(7).requiresBelowCrossCheck === true)
     assert(highestScoringWord.word === "COGGED")
   }
+
+  test("Should pass Error game state 2") {
+    val board: Board = new Board(initialiseBoard(), trie)
+
+    board.boardTiles(7)(7) = FilledBoardTile(V())
+    board.boardTiles(7)(8) = FilledBoardTile(I())
+    board.boardTiles(7)(9) = FilledBoardTile(B())
+    board.boardTiles(7)(10) = FilledBoardTile(E())
+    board.boardTiles(7)(11) = FilledBoardTile(Y())
+
+    board.boardTiles(7)(9) = FilledBoardTile(B())
+    board.boardTiles(8)(9) = FilledBoardTile(A())
+    board.boardTiles(9)(9) = FilledBoardTile(C())
+
+    board.boardTiles(9)(6) = FilledBoardTile(D())
+    board.boardTiles(9)(7) = FilledBoardTile(A())
+    board.boardTiles(9)(8) = FilledBoardTile(N())
+    board.boardTiles(9)(9) = FilledBoardTile(C())
+    board.boardTiles(9)(10) = FilledBoardTile(E())
+
+    rack.setRack(List(E(), N(), C(), L(), A(), V(), E()).to(ListBuffer))
+
+    val game: Game = new Game(board, trie, rack, bag)
+    game.printBoard()
+    game.updateBoard()
+
+    assert(board.boardTiles.length === 15)
+    assert(board.boardTiles(0).length === 15)
+
+    val highestScoringWord: HighestScoringWord = game.findHighestScoringWord(false)
+
+    assert(highestScoringWord.word === "ENCLAVE")
+    assert(highestScoringWord.x === 10)
+    assert(highestScoringWord.y === 0)
+  }
 }
